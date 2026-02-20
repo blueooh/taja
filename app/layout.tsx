f45b791aeb4a6b0a4309f1ea33a2c907910714c1
andy.oh <andy.oh@kakaocorp.com>
2026-02-20 13:52:35 +0900

    광고 meta 태그 삽입

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-9997761269602279" />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9997761269602279"
