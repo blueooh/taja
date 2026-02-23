85a79390cf8a05a0f99c3b35313f8e14e88216b0
andy.oh <andy.oh@kakaocorp.com>
2026-02-23 19:36:30 +0900

    404처리

--- app/gomoku/page.tsx
+++ app/gomoku/page.tsx
@@ -1,12 +1,5 @@
-'use client'
-
-import { useApp } from '@/lib/app-context'
-import GomokuGame from '@/components/GomokuGame'
+import { notFound } from 'next/navigation'
 
 export default function GomokuPage() {
-  const { user, onNeedAuth } = useApp()
-
-  if (user === undefined) return null
-
-  return <GomokuGame user={user} onNeedAuth={onNeedAuth} />
+  notFound()
 }
--- app/gostop/page.tsx
+++ app/gostop/page.tsx
@@ -1,12 +1,5 @@
-'use client'
-
-import { useApp } from '@/lib/app-context'
-import GostopGame from '@/components/GostopGame'
+import { notFound } from 'next/navigation'
 
 export default function GostopPage() {
-  const { user, onNeedAuth } = useApp()
-
-  if (user === undefined) return null
-
-  return <GostopGame user={user} onNeedAuth={onNeedAuth} />
+  notFound()
 }
